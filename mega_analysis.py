5ee5ccc05a653ad133e1bf6d1eeef94379776f2e
xpkang <kangxiaopeng2018@ia.ac.cn>
2020-03-12 16:50:19 +0800

    add z image

--- mega_analysis.py
+++ mega_analysis.py
@@ -31,6 +31,7 @@ def create_es_p_image(studies, n, nii, correction=False):
     uci_image = np.zeros(shape=(count))
     se_image = np.zeros(shape=(count))
     i2_image = np.zeros(shape=(count))
+    z_image = np.zeros(shape=(count))
     p_image = np.zeros(shape=(count))
 
     if not correction:
@@ -48,6 +49,7 @@ def create_es_p_image(studies, n, nii, correction=False):
             uci_image[k] = result[0][4]
             se_image[k] = result[0][6]
             i2_image[k] = result[0][9]
+            z_image[k] = z
             p_image[k] = -np.log10(p)
 
     def create_nii(image, shape, affine, header):
@@ -63,40 +65,42 @@ def create_es_p_image(studies, n, nii, correction=False):
     uci_nii = create_nii(uci_image, shape, affine, header)
     se_nii = create_nii(se_image, shape, affine, header)
     i2_nii = create_nii(i2_image, shape, affine, header)
+    z_nii = create_nii(z_image, shape, affine, header)
     p_nii = create_nii(p_image, shape, affine, header)
-    nii = nil.image.concat_imgs([es_nii, lci_nii, uci_nii, se_nii, i2_nii, p_nii])
+    nii = nil.image.concat_imgs([es_nii, lci_nii, uci_nii, se_nii, i2_nii, z_nii, p_nii])
     return nii
+
 #%%
 _mask = mask.Mask('./data/mask', 'grey_matter_smoothed_005.nii')
 d = _mask.get_mask_data().flatten()
 n = len(d[d>0])
 nii_prefix = 'mri_smoothed_removed/{}.nii'
-
-centers = ['ALL', 'ADNI']
-corrections = [True]
+centers_mcad = datasets.load_centers_mcad(use_nii=True, use_csv=False,
+                                                  nii_prefix=nii_prefix)
+centers_edsd = datasets.load_centers_edsd(use_nii=True, use_csv=False,
+                                                  nii_prefix=nii_prefix)
+centers_adni = datasets.load_centers_adni(use_nii=True, use_csv=False,
+                                                  nii_prefix=nii_prefix)
+#%%
+centers = ['ALL']
+corrections = [False]
 labels = ['NC', 'MCI', 'AD']
 pairs = [(2,0), (1, 0), (2, 1)]
-
-centers_list = datasets.load_centers_edsd(use_nii=True, use_csv=False,
-                                                  nii_prefix=nii_prefix)
-centers_list = datasets.load_centers_mcad(use_nii=True, use_csv=False,
-                                                  nii_prefix=nii_prefix)
-centers_list = datasets.load_centers_adni(use_nii=True, use_csv=False,
-                                                  nii_prefix=nii_prefix)
-
+#%%
 for center in centers:
     if center == 'EDSD':
-        centers_list = datasets.load_centers_edsd(use_nii=True, use_csv=False,
-                                                  nii_prefix=nii_prefix)
+        centers_list = centers_edsd
     elif center == 'MCAD':
-        centers_list = datasets.load_centers_mcad(use_nii=True, use_csv=False,
-                                                  nii_prefix=nii_prefix)
-    elif center == 'MCAD':
-        centers_list = datasets.load_centers_mcad(use_nii=True, use_csv=False,
-                                                  nii_prefix=nii_prefix)
+        centers_list = centers_mcad
+    elif center == 'ADNI':
+        centers_list = centers_adni
     elif center == 'ALL':
-        centers_list = datasets.load_centers_mcad(use_nii=True, use_csv=False,
-                                                  nii_prefix=nii_prefix)
+        centers_list = centers_edsd + centers_mcad + centers_adni
+
+    for _center in centers_list:
+        if len(_center.persons) <= 20:
+            centers_list.remove(_center)
+
     for pair in pairs:
         label_eg = pair[0]
         label_cg = pair[1]
