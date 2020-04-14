0a4a9b4f834cd70de97d86a33f4734e426925f16
‘e <kangxiaopeng2018@ia.ac.cn>
2020-04-14 09:08:02 +0800

    rewrite

--- correction.py
+++ correction.py
@@ -5,38 +5,33 @@ import nibabel as nib
 from nibabel import nifti1
 from scipy.stats import norm
 
-path = './report/ALL'
-files = os.listdir(path)
 
-def voxelwise_correction(array, p_array, thres=0.05):
-    voxel_count = np.size(p_array)
+def voxelwise_correction(array, p_array, voxel_count, thres=0.05):
     thresed_p = p_array < thres / voxel_count
     return np.multiply(array, thresed_p)
 
-for f in files:
-    if 'without' in f:
-        f_path = os.path.join(path, f)
-        niis =  nib.load(f_path)
-        arrays = np.asarray(niis.dataobj)
+mask_path = './data/mask/grey_matter_smoothed_005.nii'
+mask_nii = nib.load(mask_path)
+mask = np.asarray(mask_nii.dataobj)
+voxel_count = np.size(mask[mask!=0])
+#%%
+path = './results/meta'
+tests = os.listdir(path)
+for test in tests:
+    voxel_path = os.path.join(path, test, 'voxel')
+    es_path = os.path.join(voxel_path, 'es.nii')
+    p_path =  os.path.join(voxel_path, 'p.nii')
+    
+    es =  nib.load(es_path)
+    es_array = np.asarray(es.dataobj)
+    p =  nib.load(p_path)
+    p_array = np.asarray(p.dataobj)
 
-        z_array = arrays[..., -2]
-        p_array = norm.sf(z_array) * 2
-
-        for i in range(arrays.shape[-1]):
-            array = arrays[..., i]
-            corrected_array = voxelwise_correction(array, p_array)
-            arrays[..., i] = corrected_array
-        affine = niis.affine
-        header = niis.header
-        corrected_niis = nib.Nifti1Image(arrays, affine, header)
-        new_f = f.replace('without', 'with')
-        new_path = os.path.join(path, new_f)
-        nifti1.save(corrected_niis, new_path)
-
-
-# %%
-arra = np.asarray([[1,2,3,4],[5,6,7,8]])
-p_a = np.asarray([[1,0.002, 2, 3], [0.001,2, 0.001, 0.00001]])
-voxelwise_correction(arra, p_a)
+    corrected_array = voxelwise_correction(es_array, p_array, voxel_count)
+    affine = es.affine
+    header = es.header
+    corrected_niis = nib.Nifti1Image(corrected_array, affine, header)
+    new_f = os.path.join(voxel_path,'es_bon.nii')
+    nifti1.save(corrected_niis, new_f)
 
 # %%
