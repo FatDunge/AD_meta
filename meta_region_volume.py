4033b12410a761318180d824e6958ea76578808c
‘e <kangxiaopeng2018@ia.ac.cn>
2020-05-02 18:38:40 +0800

    preform meta analysis

--- meta_region_volume.py
+++ meta_region_volume.py
@@ -5,14 +5,30 @@ import numpy as np
 import os
 import csv
 from meta_analysis import utils
+from meta_analysis.main import csv_meta_analysis
 
-filenames='HYDRA.csv'
-centers = datasets.load_centers_all(filenames=filenames)
+filenames = 'origin.csv'
+centers = datasets.load_centers_mcad(filenames=filenames)
 out_path = './data/meta_csv'
-threshold = 5
-label_pairs = [(11,9), (12,9)]
-csv_prefixs = ['csv/', 'cortical_thickness/']
+threshold = 10
+label_pairs = [(2,0), (2,1), (1,0)]
+csv_prefixs = ['roi_gmv/', 'roi_ct/']
+
+def sort_models(models, filenames, orderby='es', descend=True):
+    if orderby == 'es':
+        list1 = [model.total_effect_size for model in models]
+    list1, models, filenames = (list(t) for t in zip(*sorted(zip(list1, models, filenames), reverse=descend)))
+    return list1, models, filenames
+
+def bon_cor(models, filenames,alpha=0.05):
+    after = {}
+    n = len(models)
+    for model, filename in zip(models, filenames):
+        if model.p * n <= alpha:
+            after[filename] = model
+    return after
 #%%
+# create csv for meta analysis
 for label_pair in label_pairs:
     label_eg, label_cg = label_pair
     label_dir = os.path.join(out_path, '{}_{}'.format(label_eg, label_cg))
@@ -85,32 +101,11 @@ for label_pair in label_pairs:
                                     's2': cs,
                                     'n2': cn,})
 
-# %%
-import os
-from meta_analysis.main import csv_meta_analysis
-
-def sort_models(models, filenames, orderby='es', descend=True):
-    if orderby == 'es':
-        list1 = [model.total_effect_size for model in models]
-    list1, models, filenames = (list(t) for t in zip(*sorted(zip(list1, models, filenames), reverse=descend)))
-    return list1, models, filenames
-
-def bon_cor(models, filenames,alpha=0.05):
-    after = {}
-    n = len(models)
-    for model, filename in zip(models, filenames):
-        if model.p * n <= alpha:
-            after[filename] = model
-    return after
-
-pairs = [(2,0), (1,0), (2, 1),
-         (11,9), (12,9)]
-feas = ['csv', 'cortical_thickness']
-
-for fea in feas:
-    for pair in pairs:
-        csv_dir = './data/meta_csv/{}_{}/{}'.format(pair[0], pair[1], fea)
-        out_dir = './results/meta/{}_{}/{}'.format(pair[0], pair[1], fea)
+#%%
+for csv_prefix in csv_prefixs:
+    for pair in label_pairs:
+        csv_dir = './data/meta_csv/{}_{}/{}'.format(pair[0], pair[1], csv_prefix)
+        out_dir = './results/meta/{}_{}/{}'.format(pair[0], pair[1], csv_prefix)
         if not os.path.isdir(out_dir):
             os.mkdir(out_dir)
         csvs = os.listdir(csv_dir)
@@ -123,18 +118,15 @@ for fea in feas:
             filenames.append(f[:-4])
 
         cor_model = bon_cor(models, filenames)
-
-
+        print('{}_{}:{}'.format(pair[0], pair[1], len(bon_cor(models, filenames))))
         """
-        _, models, filenames = sort_models(models, filenames, descend=False)
+        list1, models, filenames = sort_models(models, filenames, descend=False)
         top = 20
         i = 0
         for model, filename in zip(models[:top], filenames[:top]):
-            out_path = os.path.join(out_dir, '{}_{}.png'.format(filename, i))
+            out_path = os.path.join(out_dir, '{}_{}.png'.format(i, filename))
             model.plot_forest(title=filename, save_path=out_path, show=False)
             i += 1
-        
-        print('{}_{}:{}'.format(pair[0], pair[1], len(bon_cor(models, filenames))))
         """
 # %%
 import nibabel as nib
@@ -144,16 +136,16 @@ from meta_analysis import utils
 from nilearn import plotting
 from meta_analysis.main import csv_meta_analysis
 
-pairs = [(2,0), (2,1),(1,0),(11,9),(12,9)]
+pairs = [(2,0), (2,1),(1,0)]
 csv_path = './data/mask/BNA_subregions.csv'
 df = pd.read_csv(csv_path, index_col=1)
 nii_path  = './data/mask/rBN_Atlas_246_1mm.nii'
 nii = nib.load(nii_path)
-
+fea = 'roi_gmv'
 for pair in pairs:
     nii_array = np.asarray(nii.dataobj, dtype=np.float32)
-    csv_dir = './data/meta_csv/{}_{}/csv'.format(pair[0], pair[1], fea)
-    out_dir = './results/meta/{}_{}/csv'.format(pair[0], pair[1], fea)
+    csv_dir = './data/meta_csv/{}_{}/{}'.format(pair[0], pair[1], fea)
+    out_dir = './results/meta/{}_{}/{}'.format(pair[0], pair[1], fea)
     if not os.path.isdir(out_dir):
         os.mkdir(out_dir)
     csvs = os.listdir(csv_dir)
@@ -178,7 +170,6 @@ for pair in pairs:
     html_view = plotting.view_img(r)
     html_path = path[:-3]+'html'
     html_view.save_as_html(html_path)
-
 # %%
 import nilearn as nil
 from nilearn import surface
@@ -190,7 +181,7 @@ from meta_analysis import utils
 from nilearn import plotting
 from meta_analysis.main import csv_meta_analysis
 import numpy as np
-pairs = [(2,0), (2,1),(1,0),(11,9),(12,9)]
+pairs = [(2,0), (2,1),(1,0)]
 annots = ['lh.BN_Atlas.annot', 'rh.BN_Atlas.annot']
 surfs = ['lh.inflated', 'rh.inflated']
 csv_path = './data/mask/cortical_id.csv'
@@ -209,8 +200,10 @@ for pair in pairs:
         a = a.astype(np.float32)
         b = r'./data/mask/BN_Atlas_freesurfer/fsaverage/surf/{}'.format(surf)
 
-        csv_dir = './data/meta_csv/{}_{}/cortical_thickness'.format(pair[0], pair[1])
-        out_dir = './results/meta/{}_{}/cortical_thickness'.format(pair[0], pair[1])
+        csv_dir = './data/meta_csv/{}_{}/roi_ct_removed'.format(pair[0], pair[1])
+        out_dir = './results/meta/{}_{}/roi_ct_removed'.format(pair[0], pair[1])
+        if not os.path.isdir(out_dir):
+            os.mkdir(out_dir)
         csvs = os.listdir(csv_dir)
         models = []
         filenames = []
@@ -234,43 +227,6 @@ for pair in pairs:
         html_view = plotting.view_surf(b, a)
         html_path = p+'.html'
         html_view.save_as_html(html_path)
-#%%
-ll
-#%%
-import nilearn as nil
-from nilearn import surface
-from nilearn import plotting
-import nibabel as nib
-import pandas as pd
-import os
-from meta_analysis import utils
-from nilearn import plotting
-nii_path  = './data/mask/rBN_Atlas_246_1mm.nii'
-nii = nib.load(nii_path)
 
-a = surface.load_surf_data(r'E:\workspace\AD_meta\data\mask\BN_Atlas_freesurfer\fsaverage\label\rh.BN_Atlas.annot')
-b = r'E:\workspace\AD_meta\data\mask\BN_Atlas_freesurfer\fsaverage\surf\lh.inflated'
-
-# %%
-import numpy as np
-np.unique(a)
-
-# %%
-a[0]
-
-# %%
-c = surface.load_surf_data(r'E:\workspace\AD_meta\data\mask\BN_Atlas_freesurfer\fsaverage\label\lh.cortex.label')
-
-# %%
-np.unique(c)
-
-# %%
-c.size
-
-# %%
-a.size
-
-# %%
-c[c==8668857]
 
 # %%
